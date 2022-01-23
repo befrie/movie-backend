bc198b3bbe7f62a081e7147562459b1e3d993418
befrie <befrie@arcor.de>
2022-01-23 22:38:34 +0100

    updated config/middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,7 +4,21 @@ module.exports = [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
-  "strapi::query",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  ,
   "strapi::body",
   "strapi::favicon",
   "strapi::public",
