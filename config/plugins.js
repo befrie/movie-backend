0ff032436692589c2ffe0b4e90961add7c073393
befrie <befrie@arcor.de>
2022-09-05 17:46:12 +0200

    added config for graphql

--- config/plugins.js
+++ config/plugins.js
@@ -14,4 +14,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 1000,
+      apolloServer: {
+        tracing: true,
+      },
+    },
+  },
 });
